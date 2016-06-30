f9c188794b583db16aebc459f5bafc86497b738c
David Gorelik <dgore7@gmail.com>
2016-06-30 00:11:38 -0500

    fixed page refresh issues in login/signup

--- private/php/index.php
+++ private/php/index.php
@@ -12,25 +12,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sapient Ping-Pong</title>
-    <link rel="stylesheet" href="/public/css/styles.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="/css/styles.css" media="screen" title="no title" charset="utf-8">
     <link href='https://fonts.googleapis.com/css?family=Raleway:400,900' rel='stylesheet' type='text/css'>
   </head>
   <body>
     <div class="container">
       <h2>Sapient</h2>
       <h3>Ping-Pong</h3>
-      <form class="login-form" action="" method="post">
-        <input type="text" name="username" value="" placeholder="username">
-        <input type="password" name="password" value="" placeholder="password">
-        <div class="form-btns">
-          <input id="login-btn" type="submit" name="login" value="Login">
-          <input id="signup-btn" type="submit" name="signup" value="Sign Up">
-      	</div>
-        <span id="error"><?php echo $error; ?></span>
-      </form>
-      <!-- content -->
+      <div id="loader">
+
+      </div>
+      <!-- signup -->
       <script   src="https://code.jquery.com/jquery-3.0.0.min.js"   integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0="   crossorigin="anonymous"></script>
-      <script type="text/javascript" src="/public/js/index.js"></script>
+      <script type="text/javascript" src="/js/index.js"></script>
     </div>
   </body>
 </html>
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,34 @@
 // $(".container").append('<form class="login-form" action="" method="post"> <input type="text" name="username" value="" placeholder="username"> <input type="password" name="password" value="" placeholder="password"> <div class="form-btns"> <input id="login-btn" type="submit" name="login" value="Login"> <input id="signup-btn" type="submit" name="signup" value="Sign Up"> </div><span id="error"><?php echo $error; ?></span> </form>');
-$("#signup-btn").click(function(e) {
-  e.preventDefault();
-  $(".login-form").hide();
-  alert("it worked");
+// $("#signup-btn").click(function(e) {
+//   e.preventDefault();
+//   $(".login-form").hide();
+//   alert("it worked");
+// });
+var loaded= false;
+$(document).ready(function () {
+  if (!loaded) {
+    $($("#loader")).load("xml/index.html",function () {
+      loaded= true;
+      addSignUpButtonListener();
+    });
+
+  }
 });
+
+var addSignUpButtonListener = function() {
+  $("#signup-btn").click(function(e) {
+    e.preventDefault();
+    $($("#loader")).load("xml/signup.html",function () {
+      addBackButtonListener();
+    });
+  });
+};
+
+var addBackButtonListener = function() {
+  $("#back-btn").click(function(evt) {
+    evt.preventDefault();
+    $($("#loader")).load("xml/index.html",function () {
+      addSignUpButtonListener();
+    });
+  });
+};
